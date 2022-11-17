f55ee7b76a195abfac72154a3fb613fe9aedffe8
DerSchubi <schubi@erlangen.ccc.de>
2022-11-17 15:35:37 +0100

    Send welcome message when the welcome role is asigned to a user

--- welcomebot.py
+++ welcomebot.py
@@ -23,12 +23,26 @@ async def on_ready():
   print(f'Guild Members:\n - {members}\n')
 
 @client.event
+async def on_member_update(before, after):
+  guild = discord.utils.get(client.guilds, name=GUILD)
+  welcome_role = discord.utils.get(guild.roles, name="welcome")
+
+  if welcome_role in after.roles and not welcome_role in before.roles:
+    print(f'Role {welcome_role.name} was added to User {after.name}')
+    print(f'Sending Welcome Message to {after.name}...')
+    await after.create_dm()
+    await after.dm_channel.send(
+      f'Hi {after.name}!'
+    )
+    print(f'Removing Role from User...\n')
+    await after.remove_roles(welcome_role)
+
+@client.event
 async def on_message(message):
   if message.author == client.user:
     return
 
   if message.content.lower().startswith('/welcome '):
-
     print('Message incoming:')
     print(f'Author Name: {message.author.name}')
     print(f'Author ID: {message.author.id}')
@@ -47,10 +61,5 @@ async def on_message(message):
         await member.dm_channel.send(
           f'Hi {member.name}!'
         )
-
-  #if 'testing' in message.content.lower():
-  #    response = 'Huhu!'
-  #    await message.channel.send(response)
-
 
 client.run(TOKEN)
