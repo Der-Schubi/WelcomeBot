014f87a501ef0c1dacd28f4d1c4fb155efcbb3ed
DerSchubi <schubi@erlangen.ccc.de>
2024-10-15 08:34:12 +0200

    Add Newbie Role management

--- welcomebot.py
+++ welcomebot.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python -u
 # welcomebot.py
+import asyncio
 import os
 import disnake
+from datetime import datetime, timedelta
+from disnake.ext import tasks, commands
 from dotenv import load_dotenv
-from disnake.ext import commands
+from tinydb import TinyDB, Query
 
 load_dotenv()
 ENV_TOKEN = os.getenv('DISCORD_TOKEN')
@@ -11,9 +14,11 @@ ENV_GUILD = os.getenv('DISCORD_GUILD')
 ENV_REACTION = os.getenv('DISCORD_REACTION')
 ENV_COMMAND_ROLE = os.getenv('DISCORD_COMMAND_ROLE')
 ENV_WELCOME_ROLE = os.getenv('DISCORD_WELCOME_ROLE')
+ENV_NEWBIE_ROLE = os.getenv('DISCORD_NEWBIE_ROLE')
 ENV_LOG_CHANNEL = os.getenv('DISCORD_LOG_CHANNEL')
 ENV_BLACKLIST = os.getenv('DISCORD_BLACKLIST')
 ENV_STATUS = os.getenv('DISCORD_STATUS')
+ENV_NEWBIE_DAYS = os.getenv('DISCORD_NEWBIE_DAYS')
 
 blacklist = ENV_BLACKLIST.split(',')
 
@@ -31,6 +36,34 @@ bot = commands.Bot(
   help_command=None,
   #sync_commands_debug=True,
 )
+
+db = TinyDB('db.json')
+db.table('_default', cache_size=0)
+Newbies = Query() # type: tinydb.queries.Query
+
+class TimerCog(commands.Cog):
+  def __init__(self):
+    self.timer.start()
+
+  def cog_unload(self):
+    self.timer.cancel()
+
+  @tasks.loop(seconds=3600.0)
+  async def timer(self):
+    if len(bot.guilds) >= 1:
+      guild = disnake.utils.get(bot.guilds, name=ENV_GUILD)
+      channel = disnake.utils.get(guild.channels, name=ENV_LOG_CHANNEL)
+      newbie_role = disnake.utils.get(guild.roles, name=ENV_NEWBIE_ROLE)
+      current_datetime = datetime.now()
+      print(f'Searching for Users to remove the Role {newbie_role.name}. Current Datetime: {current_datetime}')
+      results = db.search(Newbies.timestamp <= current_datetime.timestamp())
+      for res in results:
+        print(f'Removing the Role {newbie_role.name} from User {res['name']}…')
+        print('Database Entry:', res) # type: tinydb.database.Document
+        user = disnake.utils.get(guild.members, id=res['id'])
+        await user.remove_roles(newbie_role)
+        db.remove(Newbies.id == user.id)
+        await channel.send(f'Removed the Role {newbie_role.name} from User {getName(user)}.')
 
 def getName(member):
   if member.nick == None:
@@ -52,20 +85,30 @@ async def on_member_update(before, after):
   guild = disnake.utils.get(bot.guilds, name=ENV_GUILD)
   channel = disnake.utils.get(guild.channels, name=ENV_LOG_CHANNEL)
   welcome_role = disnake.utils.get(guild.roles, name=ENV_WELCOME_ROLE)
+  newbie_role = disnake.utils.get(guild.roles, name=ENV_NEWBIE_ROLE)
 
-  if welcome_role in after.roles and not welcome_role in before.roles:
-    if str(after.id) in blacklist:
-      await channel.send(f'I’m sorry Dave, I’m afraid I can’t do that…')
-      print(f'User {getName(after)} is in Blacklist, cowardly refusing to send Message!')
-    else:
+  if str(after.id) in blacklist:
+    await channel.send(f'I’m sorry Dave, I’m afraid I can’t do that…')
+    print(f'User {getName(after)} is in Blacklist, cowardly refusing to send Message!')
+  else:
+    if newbie_role in after.roles and not newbie_role in before.roles:
+      print(f'Role {newbie_role.name} was added to User {getName(after)}. Adding him to Database…')
+      current_datetime = datetime.now()
+      later_datetime = current_datetime + timedelta(days = int(ENV_NEWBIE_DAYS))
+      later_datetime.timestamp()
+      timestamp = later_datetime.timestamp()
+      db.insert({'id': after.id, 'name': getName(after), 'timestamp': timestamp})
+      await channel.send(f'Role {newbie_role.name} was added to User {getName(after)} and will be removed after {ENV_NEWBIE_DAYS} Days.')
+
+    if welcome_role in after.roles and not welcome_role in before.roles:
       print(f'Role {welcome_role.name} was added to User {getName(after)}')
       print(f'Sending Welcome Message to {getName(after)}...')
       await after.create_dm()
       for message in welcome_messages:
         await after.dm_channel.send(message)
       await channel.send(f'Sent Welcome Message to {getName(after)}.')
-    print(f'Removing Role from User...\n')
-    await after.remove_roles(welcome_role)
+      print(f'Removing Welcome-Role from User...\n')
+      await after.remove_roles(welcome_role)
 
 async def is_user_qualified(inter: disnake.ApplicationCommandInteraction):
   guild = disnake.utils.get(bot.guilds, name=ENV_GUILD)
@@ -99,4 +142,6 @@ async def welcome_error(inter: disnake.ApplicationCommandInteraction, error):
 async def help(inter: disnake.ApplicationCommandInteraction) -> None:
   await inter.response.send_message(message)
 
+bot.add_cog(TimerCog())
+print('Disnake Version:', disnake.__version__)
 bot.run(ENV_TOKEN)
